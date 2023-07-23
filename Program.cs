2bdd5e8a7590523cb49fc00485fc5b0cbd9fe6ea
alhabsi16 <mxmx8283@gmail.com>
2023-07-23 10:57:36 +0400

    Threading

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace cSharp_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(doSomeWork));
+            thread.Start();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main Thread: {0}", i);
+                Thread.Sleep(1000);
+            }
+            thread.Join();
+            Console.WriteLine("Main black");
+        }
+
+        public static void doSomeWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Worker Thread: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
